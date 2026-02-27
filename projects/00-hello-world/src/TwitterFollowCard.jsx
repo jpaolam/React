7c6fc2adb42d714e3fff80c3b0368d403c037659
jpaolam <paolamartel593@gmail.com>
2026-02-26 22:48:54 -0600

    Uso de la funcion constante declarada

--- projects/00-hello-world/src/TwitterFollowCard.jsx
+++ projects/00-hello-world/src/TwitterFollowCard.jsx
@@ -1,14 +1,15 @@
 function TwitterFollowCard({userName, name}) {
+  const formatUserName = (username) => `@${username}`
   return (
     <article className='jm-followCard'>
       <header className='jm-followCard-header'>
         <img className='jm-followCard-avatar' alt='El avatar random' 
-        src='https://unavatar.io/kikobeats'></img>
+        src='https://unavatar.io/github/kikobeats'></img>
         <div className='jm-followCard-info'>
           <strong>
             {name}
           </strong>
-          <span className='jm-followCard-username'>@{userName}</span>
+          <span className='jm-followCard-username'>{formatUserName(userName)}</span>
         </div>
       </header>
       <aside>
